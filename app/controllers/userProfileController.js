28868cb769cfd271d90f21bd13c68615ee93e995
kshitijMetaBridge <kshitij@eighty5technologies.com>
2023-11-11 13:41:31 +0530

    create-profile

--- app/controllers/userProfileController.js
+++ app/controllers/userProfileController.js
@@ -1,30 +1,39 @@
 const Profile = require('../models/userProfileSchema');
 
 const createProfile = async (req, res) => {
-  try {
-    // Extract user data from the request body
-    const { fullname, email, mobileNumber, city, referralCode } = req.body;
-
-    // Create a new profile instance
-    const newProfile = new Profile({
-      fullname,
-      email,
-      mobileNumber,
-      city,
-      referralCode,
-    });
-
-    // Save the profile to the database
-    const savedProfile = await newProfile.save();
-
-    // Respond with the saved profile
-    res.json(savedProfile);
-  } catch (error) {
-    // Handle errors
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
+    try {
+      // Extract user data from the request body
+      const { fullname, email, mobileNumber, city, referralCode } = req.body;
+  
+      // Check if a profile with the given mobile number already exists
+      const existingProfile = await Profile.findOne({ mobileNumber });
+  
+      if (existingProfile) {
+        // If a profile with the mobile number already exists, respond with an error
+        return res.status(400).json({ error: 'Mobile number must be unique' });
+      }
+  
+      // Create a new profile instance
+      const newProfile = new Profile({
+        fullname,
+        email,
+        mobileNumber,
+        city,
+        referralCode,
+      });
+  
+      // Save the profile to the database
+      const savedProfile = await newProfile.save();
+  
+      // Respond with the saved profile
+      res.json(savedProfile);
+    } catch (error) {
+      // Handle errors
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  };
+  
 
 module.exports = {
   createProfile,
